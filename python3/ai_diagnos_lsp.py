40089d1206ea29fad3840cb474a466c277f34c46
Null <VectorZeroAI@gmail.com>
2026-02-08 12:18:31 +0100

    feat(model): changed the model to a better one

--- python3/ai_diagnos_lsp.py
+++ python3/ai_diagnos_lsp.py
@@ -57,7 +57,7 @@ def init_ai(api_key_input: str):
     global GeneralAnalysisChain
     global DiagnosticsOutputObjekt
     Llm = ChatOpenAI(
-            model="nvidia/nemotron-3-nano-30b-a3b:free",
+            model="tngtech/tng-r1t-chimera:free",
             api_key=SecretStr(api_key_input), 
             base_url="https://openrouter.ai/api/v1"
             )
