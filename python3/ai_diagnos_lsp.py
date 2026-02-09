9a06773a661f8a9c7b36d5a313dc540c1af5f7db
Null <VectorZeroAI@gmail.com>
2026-02-09 16:51:07 +0100

    feat(pinger in the show progress): made it show progress (count higher insdead of reapeating the message)

--- python3/ai_diagnos_lsp.py
+++ python3/ai_diagnos_lsp.py
@@ -141,11 +141,13 @@ class AI_diagnos_lsp(LanguageServer):
                 
                 # Poll until done or timeout
                 while not LangchainTimedOut:
+                    counter = 0
                     if future.done():
                         return future.result()
                     await asyncio.sleep(show_progress_every_ms / 1000)  # Check every n seconds
                     if show_progress:
-                        show_message(my_ls, "Langchain is still running")
+                        show_message(my_ls, f"Langchain is still running [{counter}]")
+                        counter = counter + 1
                 
                 # Timed out
                 return None
