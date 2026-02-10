eeb8cf9b5a6c80cbd68fa446b8c063c1f88b2895
Null <VectorZeroAI@gmail.com>
2026-02-10 11:16:16 +0100

    refactor(breaking monolit into many files): made a structure of creating a python file per Langhchain chain and refactored existing code for it

---
+++ python3/ai_diagnos_lsp/chains/BasicChainOpenrouter.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+This is the place where I will be creating the LLM (s) for the main.py file.
+"""
+
+from typing import List
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+import os
+
+from pydantic import BaseModel, SecretStr
+from pathlib import Path
+
+model = os.getenv('model_openrouter')
+assert model is not None
+api_key_openrouter = os.getenv('api_key_openrouter')
+assert api_key_openrouter is not None
+
+Llm = ChatOpenAI(
+        model=model,
+        base_url="https://openrouter.ai/api/v1/chat/completions",
+        api_key=SecretStr(api_key_openrouter)
+        )
+
+try:
+    with open(f"{Path(__file__).absolute().resolve().parent}/../prompts/general_analysis_system_prompt.txt", "r") as f:
+        GENERAL_ANALYSIS_SYSTEM_PROMPT = f.read()
+except FileNotFoundError as e:
+    raise NotImplementedError("The prompt file is missing.") from e
+
+GeneralAnalysisPrompt = ChatPromptTemplate.from_messages([
+        ("system", f"{GENERAL_ANALYSIS_SYSTEM_PROMPT}"),
+        ("human", "\n{{file_content}}\n\n"),
+        ], template_format="mustache")
+
+class DiagnosticsPydanticObjekt(BaseModel):
+    class SingleDiagnostic(BaseModel):
+
+        location: str
+        error_message: str
+        severity_level: int
+
+        # TODO : Double check if this is enough
+
+    class Config:
+        populate_by_name = True
+
+    diagnostics: List[SingleDiagnostic]
+
+GeneralDiagnosticsOutputParser = PydanticOutputParser(pydantic_object=DiagnosticsPydanticObjekt)
+
+BasicChainOpenrouter = GeneralAnalysisPrompt | Llm | GeneralDiagnosticsOutputParser
+
