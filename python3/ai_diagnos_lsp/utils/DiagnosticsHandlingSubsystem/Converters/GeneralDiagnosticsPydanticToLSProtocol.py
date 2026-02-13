7e1fdfd273df6b2602be84008a7b62419b04179d
Null <VectorZeroAI@gmail.com>
2026-02-13 11:18:28 +0100

    refactor(Diagnostics subsystem): Separated converters from Pydantic to lsprotocol into a directory.

---
+++ python3/ai_diagnos_lsp/utils/DiagnosticsHandlingSubsystem/Converters/GeneralDiagnosticsPydanticToLSProtocol.py
@@ -0,0 +1,72 @@
+from typing import List, Tuple, TYPE_CHECKING
+import logging
+import os
+from pygls.workspace import TextDocument
+from lsprotocol import types
+
+if TYPE_CHECKING:
+    from ai_diagnos_lsp.main import AI_diagnos_lsp
+
+from ai_diagnos_lsp.analysers.chains.GeneralDiagnosticsPydanticOutputParser import GeneralDiagnosticsPydanticObjekt
+from ai_diagnos_lsp.utils.grep import grep
+
+severity_map = {
+        1: types.DiagnosticSeverity.Error,
+        2: types.DiagnosticSeverity.Warning,
+        3: types.DiagnosticSeverity.Information,
+        4: types.DiagnosticSeverity.Hint
+        }
+
+
+def GeneralDiagnosticsPydanticToLSProtocol(ls: AI_diagnos_lsp,
+                                           pydantic_objekts_list: List[GeneralDiagnosticsPydanticObjekt],
+                                           document: TextDocument
+                                           ) -> List[types.Diagnostic]:
+    """
+
+    """
+    diagnostics_converted_list = []
+
+    for i in pydantic_objekts_list:
+        for j in i.diagnostics:
+
+            try:
+                if os.getenv("AI_DIAGNOS_LOG") is not None:
+                    logging.info("searching the file with grep.")
+                    logging.info(f"searching for : {j.location} ; in {document.uri}")
+                
+                if isinstance(j.location, Tuple):
+                    pos = grep(j.location[0], document.source)[j.location[1] - 1]
+                else:
+                    pos = grep(j.location, document.source)[0]
+                pos_line = pos[0]
+                pos_char = pos[1]
+                if os.getenv("AI_DIAGNOS_LOG") is not None:
+                    logging.info(f"found {j.location} at line : {pos_line}, char : {pos_char}")
+            except IndexError as e:
+                # Ignore the diagnostic entirely, because if no matches were found, it means that the AI
+                # halucinated, wich makes this one specific diagnostic is wrong, wich is not worth the hassle
+                # to try to use. So it is skipped. This is by design, not an error. 
+
+                if os.getenv("AI_DIAGNOS_LOG") is not None:
+                    logging.info(f"Errored out. Most likely a halucinated citation. The error : {e}")
+                continue 
+
+            if os.getenv("AI_DIAGNOS_LOG") is not None:
+                logging.info(f"DIAGNOSTIC : error message:  {j.error_message} ; severity level : {j.severity_level} ; pos line : {pos_line} ; pos char :  {pos_char}")
+
+            severity_level_converted = severity_map.get(j.severity_level)
+
+            diagnostics_converted_list.append(
+                    types.Diagnostic(
+                        message=j.error_message,
+                        severity=severity_level_converted,
+                        range=types.Range(
+                            start=types.Position(pos_line, pos_char),
+                            end=types.Position(pos_line, pos_char)
+                            ), 
+                        source="AI diagnos LSP", data="AI",code="AI",
+                        code_description=types.CodeDescription("This is AI generated Diagnostics. I am putting this wherever I can because why not ?")
+                        )
+                    )
+    return diagnostics_converted_list
--- python3/ai_diagnos_lsp/utils/DiagnosticsHandlingSubsystem/main.py
+++ python3/ai_diagnos_lsp/utils/DiagnosticsHandlingSubsystem/main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
 import sqlite3
 import threading
-from typing import Tuple
+from typing import Tuple, TYPE_CHECKING
 from lsprotocol import types
-from pygls.lsp.server import LanguageServer
 import time
 import logging
 import os
 
-from ai_diagnos_lsp.analysers.chains.GeneralDiagnosticsPydanticOutputParser import DiagnosticsPydanticObjekt
+from ai_diagnos_lsp.analysers.chains.GeneralDiagnosticsPydanticOutputParser import GeneralDiagnosticsPydanticObjekt
 from ai_diagnos_lsp.utils.grep import grep
-from ai_diagnos_lsp.utils.AIDiagnosLSPClassDummy import AI_Diagnos_LSP_dummy
+
+if TYPE_CHECKING:
+    from ai_diagnos_lsp.main import AI_diagnos_lsp
+
+from ai_diagnos_lsp.utils.DiagnosticsHandlingSubsystem.Converters.GeneralDiagnosticsPydanticToLSProtocol import GeneralDiagnosticsPydanticToLSProtocol
 
 class DiagnosticsHandlingSubsystemClass:
     """
@@ -20,14 +24,14 @@ class DiagnosticsHandlingSubsystemClass:
     
     Will propably get its own directory once actually implemented. 
     """
-    def __init__(self, ls: AI_Diagnos_LSP_dummy | LanguageServer, sqlite_db_name: str = "diagnostics.db", ttl_seconds: int = 2592000) -> None:
+    def __init__(self, ls: AI_Diagnos_lsp, sqlite_db_name: str = "diagnostics.db", ttl_seconds: int = 2592000) -> None:
         self.conn = sqlite3.connect(sqlite_db_name, autocommit=True)
         self.curr = self.conn.cursor()
         self.ls = ls
         self.curr.execute("""
         CREATE TABLE IF NOT EXISTS files(
             uri TEXT PRIMARY KEY,
-            hash TEXT KEY,
+            hash TEXT UNIQUE,
             last_changed_at REAL NOT NULL
             )
                           """)
@@ -53,11 +57,11 @@ class DiagnosticsHandlingSubsystemClass:
                               """, (time.time(), document_uri))
         else:
             self.curr.execute("""
-            INSERT INTO files(last_changed_at) VALUES (?) WHERE uri = ?
+            INSERT INTO files(last_changed_at, uri) VALUES (?, ?)
                               """, (time.time(), document_uri))
 
 
-    def register_new_diagnostic(self, diagnostics: DiagnosticsPydanticObjekt, document_uri: str, analysis_type: str) -> bool:
+    def register_new_diagnostic(self, diagnostics: GeneralDiagnosticsPydanticObjekt, document_uri: str, analysis_type: str) -> bool:
         self.curr.execute("""
         INSERT INTO diagnostics(uri, diagnostics, created_at) VALUES(?, ?, ?)
                           """, (document_uri, diagnostics.model_dump_json(), time.time()))
@@ -67,82 +71,47 @@ class DiagnosticsHandlingSubsystemClass:
         """
         This function DIRECTLY PUBLISHES the diagnostics for a file. 
         """
-        diagnostics_converted_list = []
-
-        json_diagnostics_list = self.curr.execute("""
-        SELECT diagnostics FROM diagnostics WHERE uri = ?
-                          """, (document_uri)).fetchall()
-        
-        pydantic_objekts_list = []
-        for i in json_diagnostics_list:
-            pydantic_objekts_list.append(
-                    DiagnosticsPydanticObjekt.model_validate_json(i)
-                    )
-
-
-        severity_map = {
-                1: types.DiagnosticSeverity.Error,
-                2: types.DiagnosticSeverity.Warning,
-                3: types.DiagnosticSeverity.Information,
-                4: types.DiagnosticSeverity.Hint
-                }
+        try:
+            json_diagnostics_list = self.curr.execute("""
+            SELECT diagnostics FROM diagnostics WHERE uri = ?
+                              """, (document_uri)).fetchall()
+            
+            pydantic_objekts_list = []
+            for i in json_diagnostics_list:
+                pydantic_objekts_list.append(
+                        GeneralDiagnosticsPydanticObjekt.model_validate_json(i)
+                        )
+        except Exception as e:
+            if os.getenv("AI_DIAGNOS_LOG") is not None:
+                logging.error("Couldnt load diagnostics")
+            return False
 
         document = self.ls.workspace.get_text_document(document_uri)
 
-        for i in pydantic_objekts_list:
-            for j in i.diagnostics:
+        diagnostics_lsprotocol_final_list = []
 
-                try:
-                    if os.getenv("AI_DIAGNOS_LOG") is not None:
-                        logging.info("searching the file with grep. ")
-                        logging.info(f"searching for : {i.location} ; in {document.uri}")
-                    
-                    if isinstance(j.location, Tuple):
-                        pos = grep(j.location[0], document.source)[j.location[1] - 1]
-                    else:
-                        pos = grep(j.location, document.source)[0]
-                    pos_line = pos[0]
-                    pos_char = pos[1]
-                    if os.getenv("AI_DIAGNOS_LOG") is not None:
-                        logging.info(f"found {j.location} at line : {pos_line}, char : {pos_char}")
-                except IndexError as e:
-                    # Ignore the diagnostic entirely, because if no matches were found, it means that the AI
-                    # halucinated, wich makes this one specific diagnostic is wrong, wich is not worth the hassle
-                    # to try to use. So it is skipped. This is by design, not an error. 
+        try:
+            diagnostics_lsprotocol_list = GeneralDiagnosticsPydanticToLSProtocol(self.ls, pydantic_objekts_list, document)
+            for i in diagnostics_lsprotocol_list:
+                diagnostics_lsprotocol_final_list.append(i)
+        except Exception as e:
+            if os.getenv("AI_DIAGNOS_LOG") is not None:
+                logging.error(f"couldnt convert diagnostics from pydantic to lsprotocol standarts due to the following error : {e}")
+            return False
 
-                    if os.getenv("AI_DIAGNOS_LOG") is not None:
-                        logging.info(f"Errored out. Most likely a halucinated citation. The error : {e}")
-                    continue 
-
-                if os.getenv("AI_DIAGNOS_LOG") is not None:
-                    logging.info(f"DIAGNOSTIC : error message:  {j.error_message} ; severity level : {j.severity_level} ; pos line : {pos_line} ; pos char :  {pos_char}")
-
-                severity_level_converted = severity_map.get(j.severity_level)
-
-                diagnostics_converted_list.append(
-                        types.Diagnostic(
-                            message=j.error_message,
-                            severity=severity_level_converted,
-                            range=types.Range(
-                                start=types.Position(pos_line, pos_char),
-                                end=types.Position(pos_line, pos_char)
-                                ), 
-                            source="AI diagnos LSP", data="AI",code="AI",
-                            code_description=types.CodeDescription(" This is AI generated Diagnostics. I am putting this wherever I can because why not ?  ")
-                            )
-                        )
-        with self.ls.diagnostics_lock:
-            self.ls.diagnostics[document_uri] = (document.version, diagnostics_converted_list)
-
+        try:
+            with self.ls.diagnostics_lock:
+                self.ls.diagnostics[document_uri] = (document.version, diagnostics_lsprotocol_final_list)
+        except Exception as e:
+            if os.getenv("AI_DIAGNOS_LOG") is not None:
+                logging.error(f"Couldnt publish diagnostics due to the following reason: {e}")
+            return False
+        
     
-
-
-
-
     def TTLBasedPruningThread(self):
-        raise NotImplementedError("Custom Diagnostics handling subsystem not yet implemented")
+        raise NotImplementedError()
 
-def DiagnosticsHandlingSubsystemFactory(ls: LanguageServer,
+def DiagnosticsHandlingSubsystemFactory(ls: AI_diagnos_lsp,
                                         sqlite_db_name: str = "diagnostics.db",
                                         ttl_seconds: int = 2592000
                                         ) -> DiagnosticsHandlingSubsystemClass:
